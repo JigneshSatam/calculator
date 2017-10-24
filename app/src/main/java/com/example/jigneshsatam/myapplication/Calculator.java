0fea926e4855ceb18882e41a346f1d48adaf3060
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2017-10-24 22:15:38 +0530

    Added calculator basic functionaity

--- app/src/main/java/com/example/jigneshsatam/myapplication/Calculator.java
+++ app/src/main/java/com/example/jigneshsatam/myapplication/Calculator.java
@@ -7,7 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Calculator extends AppCompatActivity implements View.OnClickListener{
+
+    HashMap<Character, Integer> operators = new HashMap<Character, Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,11 @@ public class Calculator extends AppCompatActivity implements View.OnClickListene
 //        ((Button) findViewById(R.id.eight)).setOnClickListener(this);
 //        ((Button) findViewById(R.id.nine)).setOnClickListener(this);
 //        ((Button) findViewById(R.id.zero)).setOnClickListener(this);
+
+        operators.put('+', 1);
+        operators.put('-', 1);
+        operators.put('*', 2);
+        operators.put('/', 2);
     }
 
     @Override
@@ -35,6 +45,7 @@ public class Calculator extends AppCompatActivity implements View.OnClickListene
         int start = result.getSelectionStart();
         int end = result.getSelectionEnd();
         String replace = null;
+        String exp = result.getText().toString();
         switch (v.getId()){
             case R.id.one :
                 replace = "1";
@@ -76,13 +87,34 @@ public class Calculator extends AppCompatActivity implements View.OnClickListene
                 replace = "0";
                 break;
 
+            case R.id.add :
+                replace = "+";
+                break;
+
+            case R.id.subtract :
+                replace = "-";
+                break;
+
+            case R.id.multiply :
+                replace = "*";
+                break;
+
+            case R.id.divide :
+                replace = "/";
+                break;
+
+            case R.id.equal :
+                String res = calculate(exp);
+                result.setText(res);
+                result.setSelection(res.length());
+                break;
+
             case R.id.clear :
                 result.setText("");
                 break;
 
         }
         if (replace == null) return;
-        String exp = result.getText().toString();
         String res = replaceAt(exp, replace, start, end);
 //        Log.d("ABC_", "length => "+ exp.length());
 //        Log.d("ABC_", "end => "+ end);
@@ -102,6 +134,77 @@ public class Calculator extends AppCompatActivity implements View.OnClickListene
         catch (Exception e){
             e.printStackTrace();
             return  original;
+        }
+    }
+
+    String calculate(String exp){
+        String result;
+        Stack<Character> operatorsStack = new Stack();
+        Stack<String> operandsStack = new Stack();
+        char[] expArray = exp.toCharArray();
+        int j = -1;
+        for(int i = 0; i < expArray.length; i++){
+            // Check if operand
+            if (Character.isDigit(expArray[i]) && j == -1){
+                j = i;
+            }
+            // Check if operator
+            else if(operators.containsKey(expArray[i])) {
+                String operand = exp.substring(j, i);
+                operandsStack.push(operand);
+                j = -1;
+                try{
+                    while (!operatorsStack.empty() && isHighPrecedence(operatorsStack.peek(), expArray[i])){
+                        solve(operandsStack, operatorsStack);
+                    }
+                    operatorsStack.push(expArray[i]);
+                }
+                catch(Exception e){
+                }
+
+            }
+        }
+
+        if(j != -1){
+            String operand = exp.substring(j, expArray.length);
+            operandsStack.push(operand);
+        }
+        while(!operatorsStack.empty()){
+            solve(operandsStack, operatorsStack);
+        }
+        return operandsStack.pop();
+    }
+
+    boolean isHighPrecedence(char topOperator, char newOperator){
+        int topValue = 0, newValue = 0;
+        topValue = operators.get(topOperator);
+        newValue = operators.get(newOperator);
+        return topValue > newValue;
+    }
+
+    void solve(Stack<String> operandsStack, Stack<Character> operatorsStack){
+        try{
+            char operator = operatorsStack.pop();
+            int op1 = Integer.parseInt(operandsStack.pop());
+            int op2 = Integer.parseInt(operandsStack.pop());
+            int value = 0;
+            switch(operator) {
+                case '+':
+                    value = op2 + op1;
+                    break;
+                case '-':
+                    value = op2 - op1;
+                    break;
+                case '*':
+                    value = op2 * op1;
+                    break;
+                case '/':
+                    value = op2 / op1;
+                    break;
+            }
+            operandsStack.push(String.valueOf(value));
+        }catch (Exception e){
+
         }
     }
 }
